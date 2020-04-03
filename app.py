06809f18c03497e77217da90f0687883940775bd
stagustine <35106100+ukonwho@users.noreply.github.com>
2020-04-03 21:23:10 +0800

    update
    update

--- app.py
+++ app.py
@@ -2,37 +2,37 @@ from web.models import User
 from web import APP, DB
 
 
-def CreateDatabase():
-    """创建数据库"""
-    DB.create_all()
-
-
-def CreateUser():
-    """创建测试账户"""
-    sql = User.query.filter(User.username == 'root').first()
-    if not sql:
-        user1 = User(username='root', password='qazxsw@123', name='管理员', phone='1388888888', email='admin@qq.com',
-                     remark='安全工程师')
-        print(user1.username)
-        DB.session.add(user1)
-        DB.session.commit()
-
-
-def DeletDb():
-    """重置数据库"""
-    DB.drop_all()
-    CreateDatabase()
-    CreateUser()
-
-
-def bayonet_main():
-    #CreateDatabase()
-    #CreateUser()
-    #APP.run(host='0.0.0.0', port=APP.config['PORT'])
-    pass
-
-
-if __name__ == '__main__':
-    bayonet_main()
-
-APP.run(host='0.0.0.0', port=APP.config['PORT'])
+#def CreateDatabase():
+#    """创建数据库"""
+#    DB.create_all()
+#
+#
+#def CreateUser():
+#    """创建测试账户"""
+#    sql = User.query.filter(User.username == 'root').first()
+#    if not sql:
+#        user1 = User(username='root', password='qazxsw@123', name='管理员', phone='1388888888', email='admin@qq.com',
+#                     remark='安全工程师')
+#        print(user1.username)
+#        DB.session.add(user1)
+#        DB.session.commit()
+#
+#
+#def DeletDb():
+#    """重置数据库"""
+#    DB.drop_all()
+#    CreateDatabase()
+#    CreateUser()
+#
+#
+#def bayonet_main():
+#    #CreateDatabase()
+#    #CreateUser()
+#    #APP.run(host='0.0.0.0', port=APP.config['PORT'])
+#    pass
+#
+#
+#if __name__ == '__main__':
+#    bayonet_main()
+#
+#APP.run(host='0.0.0.0', port=APP.config['PORT'])
