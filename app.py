39dcbc7b31f4a2864b4d4b7baee0cf25c76a9843
stagustine <35106100+ukonwho@users.noreply.github.com>
2020-04-03 20:34:37 +0800

    update app.py for test

--- app.py
+++ app.py
@@ -1,35 +1,13 @@
-from web.models import User
-from web import APP, DB
+# file_name='test.py'
+from flask import Flask
 
+app = Flask(__name__)
 
-def CreateDatabase():
-    """创建数据库"""
-    DB.create_all()
-
-
-def CreateUser():
-    """创建测试账户"""
-    sql = User.query.filter(User.username == 'root').first()
-    if not sql:
-        user1 = User(username='root', password='qazxsw@123', name='管理员', phone='1388888888', email='admin@qq.com',
-                     remark='安全工程师')
-        print(user1.username)
-        DB.session.add(user1)
-        DB.session.commit()
-
-
-def DeletDb():
-    """重置数据库"""
-    DB.drop_all()
-    CreateDatabase()
-    CreateUser()
-
-
-def bayonet_main():
-    CreateDatabase()
-    CreateUser()
-    APP.run(host='0.0.0.0', port=APP.config['PORT'])
-
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    bayonet_main()
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.run(host='0.0.0.0',port=5000)   # 设置外部访问限制，本例表示所有ip均可访问，端口为5000
